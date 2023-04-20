6ebe4d2b357c8da0a59db1e084710a6fb7132121
Thomas Perale <perale.thomas@gmail.com>
2023-04-20 23:50:18 +0200

    add primitive size getter inside the cursor class

--- src/cursor.ts
+++ src/cursor.ts
@@ -25,26 +25,51 @@ export class Cursor {
     return this.index
   }
 
+  getPrimitiveSize (primType: PrimitiveSymbol): number {
+    switch (primType) {
+      case PrimitiveSymbol.char:
+      case PrimitiveSymbol.u8:
+      case PrimitiveSymbol.i8:
+        return 1
+      case PrimitiveSymbol.u16:
+      case PrimitiveSymbol.i16:
+        return 2
+      case PrimitiveSymbol.u32:
+      case PrimitiveSymbol.i32:
+        return 4
+      // case .u64:
+      // case .i64:
+      //   return 8
+      default:
+        return 0
+    }
+  }
+
   getPrimitive (primType: PrimitiveSymbol): [number, string | number] {
+    const size = this.getPrimitiveSize(primType)
+    if (size === 0) {
+      return [0, 0]
+    }
+
     switch (primType) {
       case PrimitiveSymbol.u8:
-        return [1, this.data.getUint8(this.index)]
+        return [size, this.data.getUint8(this.index)]
       case PrimitiveSymbol.u16:
-        return [2, this.data.getUint16(this.index, this.endianness === CursorEndianness.LittleEndian)]
+        return [size, this.data.getUint16(this.index, this.endianness === CursorEndianness.LittleEndian)]
       case PrimitiveSymbol.u32:
-        return [4, this.data.getUint32(this.index, this.endianness === CursorEndianness.LittleEndian)]
+        return [size, this.data.getUint32(this.index, this.endianness === CursorEndianness.LittleEndian)]
       // case .u64:
       //   return [1, this.data.getBigUint64(this.index)];
       case PrimitiveSymbol.i8:
-        return [1, this.data.getInt8(this.index)]
+        return [size, this.data.getInt8(this.index)]
       case PrimitiveSymbol.i16:
-        return [2, this.data.getInt16(this.index, this.endianness === CursorEndianness.LittleEndian)]
+        return [size, this.data.getInt16(this.index, this.endianness === CursorEndianness.LittleEndian)]
       case PrimitiveSymbol.i32:
-        return [4, this.data.getInt32(this.index, this.endianness === CursorEndianness.LittleEndian)]
+        return [size, this.data.getInt32(this.index, this.endianness === CursorEndianness.LittleEndian)]
       // case .i64:
       //   return [1, this.data.getBigInt64(this.index)];
       case PrimitiveSymbol.char:
-        return [1, String.fromCharCode(this.data.getUint8(this.index))]
+        return [size, String.fromCharCode(this.data.getUint8(this.index))]
     }
     return [0, 0]
   }
