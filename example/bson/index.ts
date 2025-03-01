bc5f704d2aa921428de3be8cfa8f64748cf77fcd
Thomas Perale <perale.thomas@gmail.com>
2025-03-01 17:06:54 +0100

    test(Example): add bson example
    The BSON file format is a binary serialization format that can roughly be
    described as 'Binary JSON'.
    It integrates the same principle as `JSON` allowing to defines recursive object
    that can contains: arrays, numbers or string and extend those types as well.

---
+++ example/bson/index.ts
@@ -0,0 +1,20 @@
+import { promises as fs } from 'node:fs'
+import path from 'node:path'
+import { Bson } from './bson'
+
+const USAGE = `
+Usage:
+
+> deno --unstable-sloppy-imports index.ts <file>.bson
+`
+
+if (process.argv[1] === import.meta.filename) {
+  console.log(JSON.stringify(process.argv))
+  const argv = process.argv.slice(2)
+  if (argv.length == 1) {
+    const data = await fs.readFile(path.join(import.meta.dirname, argv[0]))
+    console.log(JSON.stringify(Bson.from(data.buffer).toJson(), null, 2))
+  } else {
+    console.log(USAGE)
+  }
+}
