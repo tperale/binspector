7e7186ce7a8d1d4128f80ade645882a936effd0e
Thomas Perale <perale.thomas@gmail.com>
2025-03-11 18:18:17 +0100

    test: remove the dependency to `__dirname` that doesn't exist in ESM

--- example/bson/index.test.ts
+++ example/bson/index.test.ts
@@ -1,13 +1,13 @@
-import { promises as fs } from 'node:fs'
 import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { Bson } from './bson'
 
-test('Testing BSON "helloworld.bson" read/write equality', async () => {
-  const data = await fs.readFile(path.join(__dirname, 'helloworld.bson'))
-  expect(data.buffer).binReadWriteEquality(Bson)
+test('Testing BSON "helloworld.bson" read/write equality', () => {
+  const filename = path.join(path.dirname(fileURLToPath(import.meta.url)), 'helloworld.bson')
+  expect(filename).fileReadWriteEquality(Bson)
 })
 
-test('Testing BSON "nested.bson" read/write equality', async () => {
-  const data = await fs.readFile(path.join(__dirname, 'nested.bson'))
-  expect(data.buffer).binReadWriteEquality(Bson)
+test('Testing BSON "nested.bson" read/write equality', () => {
+  const filename = path.join(path.dirname(fileURLToPath(import.meta.url)), 'nested.bson')
+  expect(filename).fileReadWriteEquality(Bson)
 })
