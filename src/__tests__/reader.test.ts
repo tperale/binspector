ab82cb4e7691ff7cb85ba09355af947d6228fd22
Thomas Perale <perale.thomas@gmail.com>
2025-03-25 21:34:44 +0100

    feat(bindump): create metadata while reading binary definition
    Metadata are created and the offset before and after reading each
    primitive will be logged to keep track of the position of each
    properties.
    
    Those metadata can be kept to dump the content of the bin.

--- src/__tests__/reader.test.ts
+++ src/__tests__/reader.test.ts
@@ -5,7 +5,7 @@ import { BinaryReader, BinaryCursorEndianness } from '../cursor.ts'
 import { CtxGet, CtxSet } from '../decorators/context.ts'
 
 function expectReadTest<Target> (buffer: Array<number>, ObjectDefinition: InstantiableObject<Target>, endian: BinaryCursorEndianness = BinaryCursorEndianness.BigEndian, ctx = {}, ...args: any[]) {
-  return expect(binread(new BinaryReader(new Uint8Array(buffer).buffer, endian), ObjectDefinition, ctx, ...args))
+  return expect(binread(new BinaryReader(new Uint8Array(buffer).buffer, endian), ObjectDefinition, ctx, {}, ...args))
 }
 
 function expectReadTestToThrow<Target> (buffer: Array<number>, ObjectDefinition: InstantiableObject<Target>) {
