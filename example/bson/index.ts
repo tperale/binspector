efd025936b8c67595ceb92244ded333c8212e860
Thomas Perale <perale.thomas@gmail.com>
2025-03-26 21:44:31 +0100

    test(Bson): add bindump example
    This change introduce the `--dump` option to the bson example
    interactive utility.
    
    It shows how to use the metadata to dump the content of a binary
    definition.

--- example/bson/index.ts
+++ example/bson/index.ts
@@ -1,19 +1,60 @@
-import { promises as fs } from 'node:fs'
+import fs from 'node:fs'
 import path from 'node:path'
 import { Bson } from './bson.ts'
+import BinDump, { type MetaBinReadClass } from '../../src/bindump.ts'
 
 const USAGE = `
-Usage:
+Usage: deno run index.ts [options] <file>.bson
 
-> deno run index.ts <file>.bson
+Show the content of a BSON binary file as a JSON object using binspector.
+
+Arguments:
+  file                BSON file to read.
+
+Options:
+  -j, --to-json       Output the content as a JSON object.
+  -d, --dump          Dump the content of the BSON binary in bindump format.
+  -h, --help          Display this help.
 `
+
+function parseArgument (argv: string[]) {
+  const result = {
+    toJson: false,
+    dump: false,
+    help: false,
+    filename: '',
+  }
+
+  argv.forEach((arg) => {
+    if (arg === '-j' || arg === '--to-json') {
+      result.toJson = true
+    } else if (arg === '-d' || arg === '--dump') {
+      result.dump = true
+    } else if (arg === '-h' || arg === '--help') {
+      result.help = true
+    } else if ((arg = path.join(import.meta.dirname, arg)) && fs.existsSync(arg)) {
+      result.filename = arg
+    }
+  })
+
+  return result
+}
 
 if (process.argv[1] === import.meta.filename) {
   console.log(JSON.stringify(process.argv))
-  const argv = process.argv.slice(2)
-  if (argv.length == 1) {
-    const data = await fs.readFile(path.join(import.meta.dirname, argv[0]))
-    console.log(JSON.stringify(Bson.from(data.buffer).toJson(), null, 2))
+  const args = parseArgument(process.argv.slice(2))
+  if (args.filename.length) {
+    const data = fs.readFileSync(args.filename)
+
+    const meta = {}
+    const bson = Bson.from(data.buffer, meta)
+    if (args.help) {
+      console.log(USAGE)
+    } else if (args.dump) {
+      console.log(BinDump.dump(data, meta as MetaBinReadClass, bson))
+    } else {
+      console.log(JSON.stringify(bson.toJson(), null, 2))
+    }
   } else {
     console.log(USAGE)
   }
